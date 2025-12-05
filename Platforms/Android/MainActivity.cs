97b68d88c6a2206f686e2e602acc62bdcaf1d5e1
brayan cortes <133264057+brayancortes22@users.noreply.github.com>
2025-12-04 22:16:59 -0500

    * 'Línea 2: Remove the using directive for Android.Content.PM as the namespace does not exist in the current project dependencies. Línea 7: Remove the ConfigurationChanges parameter as ConfigChanges is not available in the current project dependencies. This resolves the CS0103 error.' en el archivo 'Platforms\Android\MainActivity.cs'

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
-using Android.Content.PM;
 using Android.OS;
 
 namespace AutogestionSena.MAUI;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
